be1fee0ce2804f868bc0a2e53ec100e194466d9a
TownletSx <276529744@qq.com>
2024-03-20 01:44:10 +0800

    test 02

---
+++ 02逻辑运算.py
@@ -0,0 +1,29 @@
+a = 10
+b = 20
+# 判断
+print( a > b or b != a)
+
+age = 14
+if age >= 18:
+    print("可以进入网吧")
+else:
+    print("小屁孩快努力长大，就可以进网吧了")
+
+
+score = 100
+if score >= 90:
+    print("非常优秀")
+elif score >= 80 and score <= 90:
+    print("优秀")
+elif score <80 and score >= 70:
+    print("还行")
+elif score <70 and score >= 60:
+    print("及格")
+else:
+    print("小辣鸡")
+
+
+# 第二次
+c = 12
+b = "SixteenSx"
+
