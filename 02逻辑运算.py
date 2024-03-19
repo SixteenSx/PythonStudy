921038d366285159616e7e8e1ccf5bcd5c4b2621
TownletSx <276529744@qq.com>
2024-03-20 02:35:48 +0800

    if 嵌套

--- 02逻辑运算.py
+++ 02逻辑运算.py
@@ -1,29 +1,66 @@
-a = 10
-b = 20
-# 判断
-print( a > b or b != a)
+# a = 10
+# b = 20
+# # 判断
+# print( a > b or b != a)
+#
+# age = 14
+# if age >= 18:
+#     print("可以进入网吧")
+# else:
+#     print("小屁孩快努力长大，就可以进网吧了")
+#
+#
+# score = 100
+# if score >= 90:
+#     print("非常优秀")
+# elif score >= 80 and score <= 90:
+#     print("优秀")
+# elif score <80 and score >= 70:
+#     print("还行")
+# elif score <70 and score >= 60:
+#     print("及格")
+# else:
+#     print("小辣鸡")
 
-age = 14
-if age >= 18:
-    print("可以进入网吧")
-else:
-    print("小屁孩快努力长大，就可以进网吧了")
 
 
-score = 100
-if score >= 90:
-    print("非常优秀")
-elif score >= 80 and score <= 90:
-    print("优秀")
-elif score <80 and score >= 70:
-    print("还行")
-elif score <70 and score >= 60:
-    print("及格")
-else:
-    print("小辣鸡")
 
 
-# 第二次
-c = 12
-b = "SixteenSx"
+
+# if嵌套
+
+
+# 去火车站坐火车，有票进站，没票不进  安检  刀>10
+
+# piao = 1
+# knife = 9
+
+# # 错误示范
+# if piao == 1:
+#     print("进入火车站")
+# elif knife < 10:
+#     print("进入火车站")
+# else:
+#     print("请买票")
+
+# if piao == 1:
+#     print("进入火车站")
+#     if knife <10:
+#         print("可以进入")
+#     else:
+#         print("安检未通过")
+# else:
+#     print("没有票，无法进入")
+
+
+
+
+
+
+
+
+# 循环语句
+
+
+
 
