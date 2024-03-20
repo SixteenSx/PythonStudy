a970de4b81fce53550954e40f0c904ae77ed34e2
TownletSx <276529744@qq.com>
2024-03-20 20:23:35 +0800

    循环语句

--- 02逻辑运算.py
+++ 02逻辑运算.py
@@ -61,6 +61,100 @@
 
 # 循环语句
 
+# i = 0
+#
+# while 1<100:
+#     i += 1
+# print("sorry, I didn't understand{}".format(i))
+
+# i = 1
+#
+# while True:
+#     i += 1
+#     if i == 5:
+#         break
+#     print("sorry{}".format(i))
+
+
+# 1-100 累计和
+# i = 1
+# sum = 0
+# while i <= 100:
+#     sum = sum + i
+#     i += 1
+# print("1~100累积和为:%d" % sum)
+
+# 1-100 偶数累积和
+# a = 1
+# sum = 0
+# while a <= 100:
+#     if a % 2 == 0:
+#         sum = sum + a
+#     a += 1
+#
+# print("1-100偶数和为{}".format(sum))
+
+
+
+
+# for循环 break continue
+
+# name = "Sx"
+# for i in name:
+#     print(i)
+#
+# for i in range(0,10000):
+#     if i == 5:
+#         # break跳出循环
+#         # break
+#         # continue结束当次循环
+#         continue
+#     print("sorry{}".format(i))
+
+
+
+# 练习
+# 1.从一台电脑输入要出的拳 ---剪刀（1）、石头（2）、布（3）
+# 2.电脑 随机 出拳
+# 3.比较胜负
+
+# import random
+#
+# a = random.randint(1,3)
+# b = input("剪刀（1）、石头（2）、布（3）、请出拳：")
+#
+# if a == 1 and b == 3:
+#     print("win，对方出拳：{}".format(b))
+# elif a == 2 and b == 1:
+#     print("win，对方出拳：{}".format(b))
+# elif a == 3 and b == 2:
+#     print("win，对方出拳：{}".format(b))
+# elif b == a:
+#     print("平局，对方出拳：{}".format(b))
+# else:
+#     print("defeat，对方出拳：{}".format(b))
+
+
+# 参考答案
+
+# 参考答案有问题/已解决
+# player = int(input("请输入您要出的拳：石头1/剪刀2/布3:")) //必须要转换输入类型
+import random
+a = random.randint(1,10)
+print(a)
+
+player = int(input("请输入您要出的拳：石头1/剪刀2/布3:"))
+computer = random.randint(1,3)
+print(computer)
+
+if ((player == 1 and computer == 2)
+        or (player == 2 and computer == 3)
+        or (player == 3 and computer == 1)):
+    print("玩家胜利～")
+elif player == computer:
+    print("平局")
+else:
+    print("玩家失败～")
 
 
 
