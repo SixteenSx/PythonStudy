d4d3188541691bfc5f8ce2aecad5eea2bef29182
TownletSx <276529744@qq.com>
2024-03-20 21:21:37 +0800

    字符串

---
+++ 03字符串.py
@@ -0,0 +1,38 @@
+# a = 100
+# print(type(a))
+#
+# # b = "hello world"
+# # b = 'hello world'
+# # b= '''hello world'''
+# b="""hello world"""
+# print(type(b))
+#
+# # text = "Sx是世界上最\"厉害\"的人"
+# text = 'Sx是世界上最"厉害"的人'
+# print(text)
+# # '''作用 可以字符串换行
+# text2 = '''你好
+#             我我我我我我我哦我我我我'''
+# # f-string
+# name = "Sx"
+# age = "24"
+# print(f"my name is {name} ,I'm {age} years old")
+# a = 1
+# b = 2
+# print(F"a+b = {a+b}")
+
+
+
+
+
+
+# 索引
+text = 'Sx是世界上最"厉害"的人'
+print(text[0])
+print(text[2])
+# -1最后一个
+print(text[-1])
+# 切片
+print(text[0:3:])
+
+
